6c4e4d55c0cf771a81c6d3350ffa851bd281bff8
Mohamed Faaris <mohamedfaaris.dev@gmail.com>
2026-02-05 09:27:59 +0530

    cap add android

---
+++ android/app/src/main/java/com/yourcompany/mastergym/MainActivity.java
@@ -0,0 +1,5 @@
+package com.yourcompany.mastergym;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yourcompany.mastergym',
+  appName: 'Master Gym',
+  webDir: '.output/public'
+};
+
+export default config;
