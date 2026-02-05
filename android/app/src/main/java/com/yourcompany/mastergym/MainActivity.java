494e971f5a59f962faad50ba45729ceff9915a74
Mohamed Faaris <mohamedfaaris.dev@gmail.com>
2026-02-05 10:13:45 +0530

    Disables page zoom on mobile
    Disables user-initiated zoom to preserve layout and prevent UI scaling issues on mobile. On Android, turns off WebView zoom controls; in the HTML head, restricts viewport scaling via maximum-scale=1 and user-scalable=no.

--- android/app/src/main/java/com/yourcompany/mastergym/MainActivity.java
+++ android/app/src/main/java/com/yourcompany/mastergym/MainActivity.java
@@ -1,5 +1,22 @@
 package com.yourcompany.mastergym;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    WebView webView = getBridge().getWebView();
+    if (webView != null) {
+      WebSettings settings = webView.getSettings();
+      settings.setSupportZoom(false);
+      settings.setBuiltInZoomControls(false);
+      settings.setDisplayZoomControls(false);
+    }
+  }
+}
