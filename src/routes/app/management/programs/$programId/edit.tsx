e003ad89cdac065713ce42b2f51530aaab12eab5
Mohamed Faaris <mohamedfaaris.dev@gmail.com>
2026-02-15 14:39:31 +0530

    format

--- src/routes/app/management/programs/$programId/edit.tsx
+++ src/routes/app/management/programs/$programId/edit.tsx
@@ -2,16 +2,19 @@ import { createFileRoute } from '@tanstack/react-router'
 import type { Id } from '@convex/_generated/dataModel'
 import { ProgramFormScreen } from '../new'
 
-export const Route = createFileRoute('/app/management/programs/$programId/edit')(
-  {
-    component: ProgramEditRoute,
-  },
-)
+export const Route = createFileRoute(
+  '/app/management/programs/$programId/edit',
+)({
+  component: ProgramEditRoute,
+})
 
 function ProgramEditRoute() {
   const { programId } = Route.useParams()
 
   return (
-    <ProgramFormScreen mode="edit" programId={programId as Id<'trainingPlans'>} />
+    <ProgramFormScreen
+      mode="edit"
+      programId={programId as Id<'trainingPlans'>}
+    />
   )
 }
