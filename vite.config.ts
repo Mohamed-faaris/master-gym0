813af7d1946f8c94fee34ec7c390d57e8a0f8e2d
Mohamed Faaris <mohamedfaaris.dev@gmail.com>
2026-04-19 15:48:18 +0530

    deploy: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,57 @@ const config = defineConfig({
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
       allowedHeaders: ['Content-Type'],
-    },
+    }, import { URL, fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
+
+import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
+
+const config = defineConfig({
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+      },
+      server: {
+        cors: {
+          origin: '*',
+          credentials: true,
+          methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+          allowedHeaders: ['Content-Type'],
+        },
+      },
+      plugins: [
+        devtools(),
+        nitro(),
+        // this is the plugin that enables path aliases
+        viteTsConfigPaths({
+          projects: ['./tsconfig.json'],
+        }),
+        tailwindcss(),
+        tanstackStart({
+          // spa: {
+          //   enabled: true,
+          //   prerender: {
+          //     crawlLinks: true,
+          //     outputPath: 'index.html',
+          //   },
+          // },
+        }),
+        viteReact({
+          babel: {
+            plugins: ['babel-plugin-react-compiler'],
+          },
+        }),
+      ],
+    })
+
+export default config
+
   },
   plugins: [
     devtools(),
